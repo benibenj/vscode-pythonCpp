79f8f375f970ba4c22774753dff746d8d09482d5
Benjamin Simmonds <bsimmond@student.ethz.ch>
2022-07-05 15:32:12 +0200

    test

--- src/extension.ts
+++ src/extension.ts
@@ -16,27 +16,27 @@ export async function activate(context: vscode.ExtensionContext) {
 	// Check if the user has the extension installed and if so, activate them
 	extPy = vscode.extensions.getExtension("ms-python.python");
 	extCpp = vscode.extensions.getExtension("ms-vscode.cpptools");
-	if(!extPy){
-		vscode.window.showErrorMessage("You must have the official Python extension to use this debugger!!");
+	if (!extPy) {
+		vscode.window.showErrorMessage("You must have the official Python extension to use this debugger!!!");
 		return;
 	}
-	if(!extCpp){
-		vscode.window.showErrorMessage("You must have the official C++ extension to use this debugger!!");
+	if (!extCpp) {
+		vscode.window.showErrorMessage("You must have the official C++ extension to use this debugger!!!");
 		return;
 	}
-	if(!extPy.isActive){
+	if (!extPy.isActive) {
 		await extPy.activate();
 	}
-	if(!extCpp.isActive){
+	if (!extCpp.isActive) {
 		await extCpp.activate();
 	}
-			
+
 	activatePythonCppDebug(context);
-	
+
 }
 
 export function deactivate() {
-	
+
 }
 
 
